0f00e8a41e84f0ad0cb52d7d532f47c1e8df1e1b
Alvin Januar <aljan_r@outlook.co.id>
2024-05-24 01:11:51 +0700

    feat: add chat home ui

---
+++ src/lib/template/Chats.svelte
@@ -0,0 +1,23 @@
+<script>
+    import defaultImage from "$lib/assets/default_profile.jpg";
+
+    export let name = "Sample person";
+    export let lastMessage = "Sample last message";
+    export let profilePictureURL = defaultImage;
+</script>
+
+<div class="chat-list">
+    <!-- TODO: handle cases where display pictures are double, like in groups or multichats -->
+    <img class="chat-list-profile-picture" src="{profilePictureURL}" alt="{name}'s chat name display">
+    <div class="list-chat-texts">
+        <h3 class="list-chat-name">{name}</h3>
+        <span class="list-chat-content">
+            {lastMessage}
+        </span>
+    </div>
+</div>
+
+<!-- TODO: convert to LESS -->
+<style>
+    @import "style.css";
+</style>
